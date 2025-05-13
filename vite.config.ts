07c7a7d90eceaa8be44f243cb23ca34c973eed39
Ronan Igor <64883862+TioSteiNSCR@users.noreply.github.com>
2025-05-13 00:58:08 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    host: '0.0.0.0',  // Aceita conexões externas
+    port: 5173,       // A mesma porta que você está usando
+  },
+});
